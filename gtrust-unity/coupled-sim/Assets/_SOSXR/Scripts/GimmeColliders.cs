2d4b5e11aac64f67e05000f5f665629528594bd2
fwalker89 <francesco.walker89@gmail.com>
2024-08-01 14:37:30 +0200

    Prior to disable house colliders

---
+++ gtrust-unity/coupled-sim/Assets/_SOSXR/Scripts/GimmeColliders.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+
+public class GimmeColliders : MonoBehaviour
+{
+    [SerializeField] private Collider[] m_childColliders;
+    [SerializeField] private MeshCollider[] m_meshColliders;
+    [SerializeField] private BoxCollider[] m_boxColliders;
+
+
+    private void Reset()
+    {
+        m_childColliders = GetComponentsInChildren<Collider>();
+        m_meshColliders = GetComponentsInChildren<MeshCollider>();
+        m_boxColliders = GetComponentsInChildren<BoxCollider>();
+    }
+
+
+    [ContextMenu(nameof(TurnOffColliders))]
+    private void TurnOffColliders()
+    {
+        foreach (var coll in m_childColliders)
+        {
+            coll.enabled = false;
+        }
+    }
+
+
+    [ContextMenu(nameof(TurnOnColliders))]
+    private void TurnOnColliders()
+    {
+        foreach (var coll in m_childColliders)
+        {
+            coll.enabled = true;
+        }
+    }
+}
